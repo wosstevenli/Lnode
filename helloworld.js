12c1b3ea2bb5ff6c0e24ec33fc5b00f0321c5ec6
Steven li <wosstevenli@gmail.com>
2016-12-17 16:16:28 +0800

    像傻叉一样的学着Node
    像傻叉一样的学着Node

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+var http = require("http");
+
+http.createServer(function(request, response) {
+
+response.writeHead(200, {"Content-Type": "text/plain"});
+
+response.write("Hello World");
+
+response.end();
+
+}).listen(8899);
+
+console.log("nodejs start listen 8899 port!");
